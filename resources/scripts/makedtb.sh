2506229b768a0b0bd2fd5cde2e9a8d5f561da708
Jesus Acosta <jesusgabriel.91@gmail.com>
2018-11-23 13:17:04 -0400

    Updated makedtb.sh to build arm64 dt.img
    I had a 32bits device and I though this wasn't needed for 64bits devices because they already have it built and combined with the kernel into the Image.gz-dtb compressed file, but, on some sources, this file isnt generated and instead theres a single kernel Image file and we need to build the dt.img manually

--- resources/scripts/makedtb.sh
+++ resources/scripts/makedtb.sh
@@ -4,47 +4,45 @@
 # By Artx/Stayn <jesusgabriel.91@gmail.com>
 
 build_dtb () {
-echo -ne "$GREEN$BLD"
-echo -e "   ___ _____ ___  " 
-echo -e "  |   \_   _| _ ) "
-echo -e "  | |) || | | _ \ "
-echo -e "  |___/ |_| |___/ "
-echo " "
-echo " "
-echo -e "$GREEN$BLD - Build DTB Script for $VARIANT "
+  echo -ne "$GREEN$BLD"
+  echo -e "   ___ _____ ___  " 
+  echo -e "  |   \_   _| _ ) "
+  echo -e "  | |) || | | _ \ "
+  echo -e "  |___/ |_| |___/ "
+  echo " "
+  echo " "
+  echo -e "$GREEN$BLD - Build DTB Script for $VARIANT "
 
-# Check DTB Tool
-checkdtbtool
+  # Check DTB Tool
+  checkdtbtool
 
-# Remove old dt.img from kernel source
-if [ -f $P/arch/arm/boot/dt.img ]; then
-  rm $P/arch/arm/boot/dt.img &> /dev/null
-fi
+  # Remove old dt.img from kernel source
+  if [ -f $P/arch/$ARCH/boot/dt.img ]; then
+   rm $P/arch/$ARCH/boot/dt.img
+  fi
 
-# Build with Lineage dtbTool
-if [ -f $DTB ]; then
+  # Build with Lineage dtbTool 
   chmod 777 $DTB
   echo " "
   echo -ne "$WHITE   Building DTB with dtbToolLineage...$RATT$WHITE"
-  $DTB -2 -o $P/arch/arm/boot/dt.img -s 2048 -p $P/scripts/dtc/ $P/arch/arm/boot/ &> $LOGF/build-dtb_log.txt
-fi
+  $DTB -2 -o $P/arch/$ARCH/boot/dt.img -s 2048 -p $P/scripts/dtc/ $P/arch/$ARCH/boot/ &> $LOGF/build-dtb_log.txt
 
-# Verify dt.img
-if [ ! -f $P/arch/arm/boot/dt.img ]; then
-  echo " "
-  echo -e "$RED Create dt.img failed!$RATT$WHITE"
-  read -p "   Read build-dtb_log? [y/n]: " RDDTB
-  if [ $RDDTB = y ] || [ $RDDTB = Y ]; then
-    nano $LOGF/build-dtb_log.txt
-    unset RDDTB
+  # Verify dt.img
+  if [ ! -f $P/arch/$ARCH/boot/dt.img ]; then
+    echo " "
+    echo -e "$RED Error: DTB Build failed or no unique DTB(s) were found$RATT$WHITE"
+    read -p "   Read build-dtb_log? [y/n]: " RDDTB
+    if [ $RDDTB = y ] || [ $RDDTB = Y ]; then
+     nano $LOGF/build-dtb_log.txt
+     unset RDDTB
+    fi
+    echo -e "$RATT"
+    return 1
+  else
+   mv $P/arch/$ARCH/boot/dt.img $DT/$VARIANT
+   echo -e "$GREEN$BLD Done$RATT"
+   echo " "
   fi
-  echo -e "$RATT"
-  return 1
-else
-  mv $P/arch/arm/boot/dt.img $DT/$VARIANT
-  echo -e "$GREEN$BLD Done$RATT"
-  echo " "
-fi
 }
 
 # Done
