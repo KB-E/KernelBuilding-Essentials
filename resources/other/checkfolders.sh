c469088ba72a2b47bd323fc17308cbaf6d9be420
Artx-II <jesusgabriel.91@gmail.com>
2019-04-24 16:08:30 +0000

    add checkfolders.sh

---
+++ resources/other/checkfolders.sh
@@ -0,0 +1,26 @@
+checkfolders () {
+  # Check environment folders and if one, some or all doesnt exist
+  # create or restore it
+  if [ "$1" != "--silent" ]; then
+    echo " "
+    echo -ne "$GREEN$BLD - Checking Enviroment Folders... ";
+  fi
+  sleep 0.5
+  folder () {
+    if [ ! -d $CDF/$FD ]; then
+      mkdir $CDF/$FD;
+    fi
+  }
+  FD=out; folder
+  FD=out/Images; folder
+  FD=out/dt; folder
+  FD=source; folder
+  FD=resources/logs; folder
+  FD=resources/devices; folder
+  unset FD
+  if [ "$1" != "--silent" ]; then
+    echo -e "$WHITE Done$RATT"
+  fi
+}
+export -f checkfolders
+
