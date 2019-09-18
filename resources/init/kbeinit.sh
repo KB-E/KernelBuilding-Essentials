7d91a32efce3ee02d1a6075ef2c32c77b3ceee76
Jesus Acosta <jesusgabriel.91@gmail.com>
2019-09-18 20:45:19 +0000

    KB-E: Add custom kbeinit.sh script
    Add the path for KB-E Logs and source
    the simpletools.sh script

---
+++ resources/init/kbeinit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# KB-E custom init script
+# By Artx/Stayn <artx4dev@gmail.com>
+
+# Export KB-E Log path
+export KBELOG=$CDF/resources/logs/kbessentials.log
+
+# Load simpletools
+source $CDF/resources/simpletools.sh
