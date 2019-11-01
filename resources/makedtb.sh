99ea00269cb015eaab99b4a2f2e911f07fac4594
Jesus Acosta <jesusgabriel.91@gmail.com>
2019-11-01 10:46:52 -0400

    Makedtb: Update script

--- resources/makedtb.sh
+++ resources/makedtb.sh
@@ -5,16 +5,16 @@
 
 function compiledtb() {
   checkdtbtool
-  DTBF=$CDF/resources/dtbtool/dtbtool.c
-  DTBB=$CDF/resources/dtbtool/dtbtool
-  DTBC=$CDF/resources/dtbtool/dtbtool.o
-  if [ ! -f $DTBB ]; then
+  dtb_code=$kbe_path/resources/dtbtool/dtbtool.c
+  dtb_binary=$kbe_path/resources/dtbtool/dtbtool
+  dtb_compiled=$kbe_path/resources/dtbtool/dtbtool.o
+  if [ ! -f $dtb_binary ]; then
     echo -e "$WHITE   Compiling DTB Tool..."; kbelog -t "CompileDTB: Compiling DTB Tool..."
-    gcc -c $DTBF -o $DTBC; if [ -f $DTBC ]; then kbelog -t "CompileDTB: dtbtool.o found"; fi
-    gcc $DTBC -o dtbtool && mv dtbtool $(dirname $DTBB); if [ -f $DTBB ]; then kbelog -t "CompileDTB: dtbtool build done"; fi
-    if [ ! -f $DTBB ]; then
+    gcc -c $dtb_code -o $dtb_compiled; if [ -f $dtb_compiled ]; then kbelog -t "CompileDTB: dtbtool.o found"; fi
+    gcc $dtb_compiled -o dtbtool && mv dtbtool $(dirname $dtb_binary); if [ -f $dtb_binary ]; then kbelog -t "CompileDTB: dtbtool build done"; fi
+    if [ ! -f $dtb_binary ]; then
       echo -e "$RED$BLD   Error: DTB Tool compile failed"
-      export NODTB=1; kbelog -t "CompileDTB: DTB Tool compile failed"
+      export dtb_compile_failed=true; kbelog -t "CompileDTB: DTB Tool compile failed"
     fi
     echo -e "$WHITE   Done$RATT"
   else
@@ -24,7 +24,7 @@ function compiledtb() {
 export -f compiledtb; kbelog -f compiledtb
 
 function makedtb() {
-  DTB=$CDF/resources/dtbtool/dtbtool
+  DTB=$kbe_path/resources/dtbtool/dtbtool
   echo -ne "$THEME$BLD"
   echo -e "   ___ _____ ___  " 
   echo -e "  |   \_   _| _ ) "
@@ -45,9 +45,9 @@ function makedtb() {
   kbelog -t "MakeDTB: Building DTB with dtbToolLineage"
   echo -e "$WHITE   Building DTB...$RATT$WHITE"
   if [ $kernel_arch = arm ]; then
-    $DTB -2 -o $kernel_source/arch/$kernel_arch/boot/dt.img -s 2048 -p $kernel_source/scripts/dtc/ $kernel_source/arch/$kernel_arch/boot/ &> $LOGF/build-dtb_log.txt
+    $DTB -2 -o $kernel_source/arch/$kernel_arch/boot/dt.img -s 2048 -p $kernel_source/scripts/dtc/ $kernel_source/arch/$kernel_arch/boot/ &> $kbe_path/logs/build-dtb_log.txt
   elif [ $kernel_arch = arm64 ]; then
-    $DTB -2 -o $kernel_source/arch/$kernel_arch/boot/dt.img -s 2048 -p $kernel_source/scripts/dtc/ $kernel_source/arch/$kernel_arch/boot/dts/qcom/ &> $LOGF/build-dtb_log.txt
+    $DTB -2 -o $kernel_source/arch/$kernel_arch/boot/dt.img -s 2048 -p $kernel_source/scripts/dtc/ $kernel_source/arch/$kernel_arch/boot/dts/qcom/ &> $kbe_path/logs/build-dtb_log.txt
   fi
 
   # Create out folder for this device
@@ -59,15 +59,9 @@ function makedtb() {
   if [ ! -f $kernel_source/arch/$kernel_arch/boot/dt.img ]; then
     echo " "; kbelog -t "MakeDTB: Error: DTB Build failed, exiting..."
     echo -e "$RED Error: DTB Build failed or no unique DTB(s) were found$RATT$WHITE"
-    read -p "   Read build-dtb_log? [y/n]: " RDDTB
-    if [ $RDDTB = y ] || [ $RDDTB = Y ]; then
-     kbelog -t "MakeDTB: Opening DTB Build kbelog to user"
-     nano $LOGF/build-dtb_log.txt
-     unset RDDTB
-    fi
     echo -e "$RATT"
     # Report DTB Build failed to KB-E
-    export DTBFAILED=1
+    export dtb_build_failed=true
     return 1
   else
    mv $kernel_source/arch/$kernel_arch/boot/dt.img $DTOUT/$device_variant; kbelog -t "MakeDTB: New DTB moved to '$DTOUT' named '$device_variant'"
