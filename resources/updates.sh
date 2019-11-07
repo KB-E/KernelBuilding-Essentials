d4a187363e1e65837ea4e846f8a28e5bdb532bf4
Jesus Acosta <jesusgabriel.91@gmail.com>
2019-11-06 20:58:24 -0400

    updates: Clear updates.sh script

--- resources/updates.sh
+++ resources/updates.sh
@@ -3,14 +3,3 @@
 # Updater Script
 # By Artx/Stayn <jesusgabriel.91@gmail.com>
 
-if grep -q "# Load KB-E Function and Path" ~/.bashrc; then
-  # Remove old code from ~/.bashrc
-  sudo sed -i '/# Load KB-E Function and Path/d' ~/.bashrc
-  sudo sed -i '/CDF=/d' ~/.bashrc
-  sudo sed -i '/colors.sh/d' ~/.bashrc
-  sudo sed -i '/core.sh/d' ~/.bashrc
-  sudo sed -i '/log.sh/d' ~/.bashrc
-  sudo sed -i "/complete -W 'start upgrade' kbe/d" ~/.bashrc
-  sudo sed -i "/complete -W 'start update' kbe/d" ~/.bashrc
-fi
-
