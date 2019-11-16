72524edc35eb09701c6668bf60f750e07a3008c5
Jesus Acosta <jesusgabriel.91@gmail.com>
2019-11-16 01:43:38 +0000

    KB-E: Installation methods CLEANUP

--- kbe.sh
+++ kbe.sh
@@ -22,43 +22,16 @@ if [ ! -f resources/other/firstrun ] && [ "$1" != --init ]; then
   # Display Disclaimer
   disclaimer=$(cat resources/setup/disclaimer.txt); echo " "; echo $disclaimer; unset disclaimer
   echo " "; read -p " - Do you agree the above disclaimer and continue? [Y/N]: " DAG; echo " " 
-  # Exit the pre-installation with variable "agreed_disclaimer" set to false
-  # for the core script if user doesn't like it >:(
+  # Exit the pre-installation if user doesn't like it >:(
   if [ "$DAG" != "y" ] && [ "$DAG" != "Y" ]; then
-    export agreed_disclaimer=false; unset DAG; return 1
-  fi; unset DAG
-  if [ "$agreed_disclaimer" = "false" ]; then
-    return 1
+    unset DAG; return 1
   fi
   read -p "   Thanks, good luck with your builds! Press enter to continue..."; echo " "
+  # Initialize the pre-installation Script
   source resources/setup/preinstall.sh
-  # -------------------------------------
-  # Make some preparations before install
-  # -------------------------------------
-  # Set permissions
-#  sudo chown -R $USER:users *
-  # Export current full path to KB-E
-#  export kbe_path=$(pwd)
-  # Create KB-E Environment folders
-#  folders=(devices logs source)
-#  for i in "${folders[@]}"; do
-#    if [ ! -d $i ]; then
-#      mkdir $i
-#    fi
-#  done; unset folders
-  # Load log script
-#  source resources/log.sh
-  # Title
-#  source resources/other/programtitle.sh
-  # Load Colors
-#  source resources/other/colors.sh
-  # -------------------------
-  # All set, ready to install
-  #  -------------------------
   kbelog -t "KB-E: Pre-Installation is done"
-  # Initialize the Installation Script
-  source resources/setup/preinstall.sh
-  source resources/setup/install.sh |& tee -a logs/install_log.txt
+  # Initialize the installation Script
+  source resources/setup/install.sh
   kbelog -t "KB-E: Installation is done"
 fi
 
--- resources/setup/preinstall.sh
+++ resources/setup/preinstall.sh
@@ -1,23 +1,27 @@
-  # -------------------------------------
-  # Make some preparations before install
-  # -------------------------------------
-  # Set permissions
-  sudo chown -R $USER:users *
-  # Export current full path to KB-E
-  export kbe_path=$(pwd)
-  # Create KB-E Environment folders
-  folders=(devices logs source)
-  for i in "${folders[@]}"; do
-    if [ ! -d $i ]; then
-      mkdir $i
-    fi
-  done; unset folders
-  # Load log script
-  source resources/log.sh
-  # Title
-  source resources/other/programtitle.sh
-  # Load Colors
-  source resources/other/colors.sh
-  # -------------------------
-  # All set, ready to install
-  #  -------------------------
+#!/bin/bash
+# KB-E Pre-Installation Script
+# By Artx <artx4dev@gmail.com>
+
+# -------------------------------------
+# Make some preparations before install
+# -------------------------------------
+# Set permissions
+sudo chown -R $USER:users *
+# Export current full path to KB-E
+export kbe_path=$(pwd)
+# Create KB-E Environment folders
+folders=(devices logs source)
+for i in "${folders[@]}"; do
+  if [ ! -d $i ]; then
+    mkdir $i
+  fi
+done; unset folders
+# Load log script
+source resources/log.sh
+# Title
+source resources/other/programtitle.sh
+# Load Colors
+source resources/other/colors.sh
+# -------------------------
+# All set, ready to install
+#  -------------------------
